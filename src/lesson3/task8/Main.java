43a0c3bd14749b37a77bab88bc2ee11fde588583
KateWi <katya539063@yandex.ru>
2024-03-04 23:23:11 +0300

    com13

---
+++ src/lesson3/task8/Main.java
@@ -0,0 +1,11 @@
+package lesson3.task8;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 98) {
+            System.out.print(i+7+" ");
+            i= i + 7;
+        }
+    }
+}
