656518899ac39a9f3a6193c760754b15211d6819
KateWi <katya539063@yandex.ru>
2024-03-04 23:10:17 +0300

    com12

---
+++ src/lesson3/task7/Main.java
@@ -0,0 +1,19 @@
+package lesson3.task7;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Введите целое положительное число:");
+        Scanner sc = new Scanner(System.in);
+        int g = sc.nextInt();
+        int i = 1;
+        if (g > 0) {
+            while (i <= g) {
+                System.out.println(i++);
+            }
+        }
+        else {System.out.println("Введено неверное число");
+        }
+    }
+}
