35469b40d3d75bb7564c56cb33e77d719edaecd4
KateWi <katya539063@yandex.ru>
2024-03-04 22:49:46 +0300

    com11

---
+++ src/lesson3/task6/Main.java
@@ -0,0 +1,9 @@
+package lesson3.task6;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i=1; i < 100; i = i + 2) {
+            System.out.println(i);
+        }
+    }
+}
