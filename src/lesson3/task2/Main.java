87be6e8611a1d1cb775f8bda6c9b381de2e88dda
KateWi <katya539063@yandex.ru>
2024-03-05 16:31:55 +0300

    com21

--- src/lesson3/task2/Main.java
+++ src/lesson3/task2/Main.java
@@ -6,13 +6,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int g = sc.nextInt();
-        if ((g < 3 & g > 0) | (g == 12)) {
+        if ((g < 3 && g > 0) | (g == 12)) {
             System.out.println("Winter");
-        } else if (g > 2 & g < 6) {
+        } else if (g > 2 && g < 6) {
             System.out.println("Spring");
-        } else if (g > 5 & g < 9) {
+        } else if (g > 5 && g < 9) {
             System.out.println("Summer");
-        } else if (g > 8 & g < 12) {
+        } else if (g > 8 && g < 12) {
             System.out.println("Autumn");
         } else {
             System.out.println("Invalid month number");
