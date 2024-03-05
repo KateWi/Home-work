64a63fd9fb17f98b9a3a8b3095e42e246e266337
KateWi <katya539063@yandex.ru>
2024-03-05 16:05:55 +0300

    com19

--- src/lesson3/task7/Main.java
+++ src/lesson3/task7/Main.java
@@ -7,11 +7,15 @@ public class Main {
         System.out.println("Введите целое положительное число:");
         Scanner sc = new Scanner(System.in);
         int g = sc.nextInt();
-        int i = 1;
+        int i = 0;
+        int sum = 0;
         if (g > 0) {
-            while (i <= g) {
-                System.out.println(i++);
+            while (i <  g) {
+                sum = sum + g;
+               // System.out.println("Сумма: " + sum);
+                g--;
             }
+            System.out.println("Сумма: " + sum);
         }
         else {System.out.println("Введено неверное число");
         }
--- src/lesson3/task8/Main.java
+++ src/lesson3/task8/Main.java
@@ -3,9 +3,9 @@ package lesson3.task8;
 public class Main {
     public static void main(String[] args) {
         int i = 0;
-        while (i < 98) {
-            System.out.print(i+7+" ");
+           while (i < 98) {
             i= i + 7;
+            System.out.print(i + " ");
         }
     }
 }
