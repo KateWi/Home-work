0a6be33641e6a89a60ee85bb4feadc1b364a2611
KateUrbanovich <73164321+KateWi@users.noreply.github.com>
2024-03-28 01:24:05 +0300

    Delete src/lesson3 directory

--- src/lesson3/task6/Main.java
+++
@@ -1,9 +0,0 @@
-package lesson3.task6;
-
-public class Main {
-    public static void main(String[] args) {
-        for (int i = 1; i < 100; i = i + 2) {
-            System.out.println(i);
-        }
-    }
-}
